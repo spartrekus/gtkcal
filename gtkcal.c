91c1c48acf552bbc01f08251b60cb7b0567a0fbe
spartrekus <spartrekus@gmail.com>
2018-12-15 07:15:56 +0100

    Update gtkcal.c

--- gtkcal.c
+++ gtkcal.c
@@ -2,6 +2,7 @@
 // sudo apt-get install libgtk2.0-dev
 // gcc gtkcal.c -o gtkcal `pkg-config --cflags --libs gtk+-2.0` 
 
+// it will need to compile: 
 /// -pthread -I/usr/include/gtk-2.0 -I/usr/lib/i386-linux-gnu/gtk-2.0/include -I/usr/include/gio-unix-2.0/ -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/libpng16 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libpng16 -I/usr/include/pango-1.0 -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/glib-2.0 -I/usr/lib/i386-linux-gnu/glib-2.0/include -I/usr/include/freetype2 -lgtk-x11-2.0 -lgdk-x11-2.0 -lpangocairo-1.0 -latk-1.0 -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lpangoft2-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lfontconfig -lfreetype
 
 
